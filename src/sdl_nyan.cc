52cf8b821ff28d7a29536dccfd1a0e019a7636ee
oxmox <badctoxymoron@gmx.de>
2023-03-11 07:27:05 +0100

    add new make_nyan_sprite_sheet_from_mem() function

--- src/sdl_nyan.cc
+++ src/sdl_nyan.cc
@@ -10,6 +10,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#include "nyan_data_r.c"
+
 static const char *const NYAN_PATH = "../external/nyan/nyan";
 static const size_t NYAN_SPRITE_COUNT = 12;
 static const size_t NYAN_SPRITE_WIDTH = 36;
@@ -47,30 +49,64 @@ static constexpr SDL_Rect nyan_sheet_rect()
     return { 0, 0, NYAN_SPRITE_COUNT * NYAN_SPRITE_WIDTH, NYAN_SPRITE_HEIGHT };
 }
 
-static SDL_Texture *make_nyan_sprite_sheet(SDL_Renderer *renderer, const char dir = 'r')
+static SDL_Texture *make_nyan_sprite_sheet_from_files(SDL_Renderer *renderer, const char dir = 'r')
 {
     static const auto textureRect = nyan_sheet_rect();
     auto result = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
                                     textureRect.w, textureRect.h);
 
     if (!result)
-        nyan_sdl_fatal("make_nyan_sprite_sheet/SDL_CreateTexture");
+        nyan_sdl_fatal("make_nyan_sprite_sheet_from_files/SDL_CreateTexture");
 
     if (SDL_SetTextureBlendMode(result, SDL_BLENDMODE_BLEND))
-        nyan_sdl_fatal("make_nyan_sprite_sheet/SDL_SetTextureBlendMode");
+        nyan_sdl_fatal("make_nyan_sprite_sheet_from_files/SDL_SetTextureBlendMode");
 
     std::array<char, 1024> strBuf;
 
     for (size_t i=0; i<NYAN_SPRITE_COUNT; ++i)
     {
         auto filename = aprintf(strBuf, "%s/%c%zu.png", NYAN_PATH, dir, i+1);
-        log_trace("make_nyan_sprite_sheet: loading %s", filename);
+        log_trace("make_nyan_sprite_sheet_from_files: loading %s", filename);
         int w = 0, h = 0, bytes_per_pixel = 0;
         u8 *data = stbi_load(filename, &w, &h, &bytes_per_pixel, 0);
         assert(w == NYAN_SPRITE_WIDTH && h == NYAN_SPRITE_HEIGHT && bytes_per_pixel == NYAN_BBP);
         auto destRect = nyan_sprite_rect(i);
         if (SDL_UpdateTexture(result, &destRect, data, NYAN_BBP * NYAN_SPRITE_WIDTH))
-            nyan_sdl_fatal("make_nyan_sprite_sheet/SDL_UpdateTexture");
+            nyan_sdl_fatal("make_nyan_sprite_sheet_from_files/SDL_UpdateTexture");
+        STBI_FREE(data);
+    }
+
+    return result;
+}
+
+static SDL_Texture *make_nyan_sprite_sheet_from_mem(SDL_Renderer *renderer)
+{
+    static const uint8_t *const data_ptrs[] = { r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, nullptr };
+
+    static const size_t data_sizes[] = {
+            sizeof(r1), sizeof(r2), sizeof(r3), sizeof(r4), sizeof(r5), sizeof(r6),
+            sizeof(r7), sizeof(r8), sizeof(r9), sizeof(r10), sizeof(r11), sizeof(r12)
+    };
+
+    static const auto textureRect = nyan_sheet_rect();
+    auto result = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
+                                    textureRect.w, textureRect.h);
+
+    if (!result)
+        nyan_sdl_fatal("make_nyan_sprite_sheet_from_mem/SDL_CreateTexture");
+
+    if (SDL_SetTextureBlendMode(result, SDL_BLENDMODE_BLEND))
+        nyan_sdl_fatal("make_nyan_sprite_sheet_from_mem/SDL_SetTextureBlendMode");
+
+    for (size_t i=0; data_ptrs[i]; ++i)
+    {
+        log_trace("make_nyan_sprite_sheet_from_files: loading data %zu", i);
+        int w = 0, h = 0, bytes_per_pixel = 0;
+        u8 *data = stbi_load_from_memory(data_ptrs[i], data_sizes[i], &w, &h, &bytes_per_pixel, 0);
+        assert(w == NYAN_SPRITE_WIDTH && h == NYAN_SPRITE_HEIGHT && bytes_per_pixel == NYAN_BBP);
+        auto destRect = nyan_sprite_rect(i);
+        if (SDL_UpdateTexture(result, &destRect, data, NYAN_BBP * NYAN_SPRITE_WIDTH))
+            nyan_sdl_fatal("make_nyan_sprite_sheet_from_files/SDL_UpdateTexture");
         STBI_FREE(data);
     }
 
@@ -155,7 +191,8 @@ int main(int argc, char *argv[])
     if (!renderer)
         nyan_sdl_fatal("SDL_CreateRenderer");
 
-    auto nyanSheet = make_nyan_sprite_sheet(renderer);
+    //auto nyanSheet = make_nyan_sprite_sheet_from_files(renderer);
+    auto nyanSheet = make_nyan_sprite_sheet_from_mem(renderer);
 
     NyanSpinnyCircle nsc;
     nsc.nyanSheet = nyanSheet;
